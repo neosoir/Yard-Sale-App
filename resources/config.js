ce0b9269b2c673e608132ff908b14bc6b5566d41
Neo <neo@neoslab.dev>
2025-03-16 17:09:44 -0600

    [add] make requesto to api using fetch

--- resources/config.js
+++ resources/config.js
@@ -1,23 +1,10 @@
-const url = process.env.EXPO_PUBLIC_SITE_URL;
-const consumerKey = process.env.EXPO_PUBLIC_CUSTOMER_KEY;
-const consumerSecret = process.env.EXPO_PUBLIC_CUSTOMER_SECRET;
-const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+const siteUrl = process.env.EXPO_PUBLIC_SITE_URL;
+const customerKey = process.env.EXPO_PUBLIC_CUSTOMER_KEY;
+const customerSecret = process.env.EXPO_PUBLIC_CUSTOMER_SECRET;
 
+const config = {
+  siteUrl: `${siteUrl}wp-json/wc/v3/`,
+  wcCredentials: `consumer_key=${customerKey}&consumer_secret=${customerSecret}`,
+};
 
-console.log('url', url)
-console.log('consumerKey', consumerKey)
-console.log('consumerSecret', consumerSecret)
-
-
-const api = new WooCommerceRestApi({
-  url,
-  consumerKey,
-  consumerSecret,
-  version: 'wc/v3',
-  queryStringAuth: true // Añadir esta línea para conexiones HTTP
-});
-
-console.log('api', api)
-
-
-export default api;
+export default config;
