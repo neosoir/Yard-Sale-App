2b8f0cce90dd95a524766a31eeb8fe75f847bf98
Neo <neo@neoslab.dev>
2025-03-17 13:27:49 -0600

    [add] move from js to ts and tsx files

--- context/GlobalState.tsx
+++ context/GlobalState.tsx
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 
 import Context from './Context';
-import config from '../resources/config';
 import apiFetch from '../resources/apiFetch';
 
 const GlobalState = props => {
